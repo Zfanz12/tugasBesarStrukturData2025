2de8c93502fafc031330ac2e238257ed6d557e5b
Zfanz12 <zulfanfalah@gmail.com>
2025-12-11 01:42:45 +0700

    menambah bbrp logika di cpp

--- tubes/tubes.cpp
+++ tubes/tubes.cpp
@@ -1,11 +1,46 @@
 #include "tubes.h"
 
-void createListDivisi(adrDivisi &L);
-void createListProject(adrProject &L);
-adrDivisi newDivisi(divisi x);
-adrPanitia newPanitia(panitia x);
-adrProject newProject(project x);
-adrRelasi newRelasi(adrProject P);
+void createListDivisi(adrDivisi &L) {
+    createListDivisi(L);
+    L = nullptr;
+}
+void createListProject(adrProject &L) {
+    createListProject(L);
+    L =nullptr;
+}
+
+adrDivisi newDivisi(divisi x) {
+    adrDivisi P = new elmDivisi;
+    P->info = x;
+    P->next= nullptr;
+    P->firstPanitia = nullptr;
+    return P'
+}
+
+adrPanitia newPanitia(panitia x) {
+    adrPanitia P = new elmPanitia;
+    P->info = x;
+    P->next = nullptr;
+    P->prev = nullptr;
+    P->firstProject = nullptr;
+    return P;
+
+}
+adrProject newProject(project x) {
+    adrProject P = new elmProject;
+    P->info = x;
+    P->next = nullptr;
+    return P;
+}
+
+adrRelasi newRelasi(adrProject P) {
+    adrRelasi P = new elmRelasi;
+    P->project = P;
+    P->next = nullptr;
+    P->prev = nullptr;
+    return P;
+}
+
 void insertLastDivisi(adrDivisi &L, adrDivisi P);
 void insertPanitia(adrDivisi D, adrPanitia P);
 void insertLastProject(adrProject &L, adrProject P);
